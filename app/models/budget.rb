20fa71da80b8a4c15b848f26046b68e8f098d482
KevJSDevelopment <Kshhn12@gmail.com>
2020-11-19 22:02:51 -0500

    small updates

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -4,12 +4,10 @@ class Budget < ApplicationRecord
     belongs_to :user
 
     def get_expense_info(expenses_arr)
-        # byebug
         if expenses_arr.length > 0
             categories = expenses_arr.map do |expense|
                 { cat: expense.expense_category, expenses: expense.expense_category.expenses }
             end
-            # byebug
             return categories.uniq { |e| e[:cat] }
         else
             categories = []
