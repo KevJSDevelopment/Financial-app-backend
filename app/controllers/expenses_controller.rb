915c1c8abc0526f1df53d74102679c26bc472e61
KevJSDevelopment <Kshhn12@gmail.com>
2020-11-22 17:53:30 -0500

    added expense uupdate route, can update expense categories

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -10,8 +10,26 @@ class ExpensesController < ApplicationController
             }
         else
             render json: {
-                auth: true,
+                auth: false,
                 message: expense.error.full_messages
+            }
+        end
+    end
+
+    def update
+        expense = Expense.find(params[:id])
+        # byebug
+        if expense
+            expense.update(expense_category_id: params[:expense_category_id])
+            render json: {
+                expense: expense,
+                auth: true,
+                message: "Expense #{expense.id} Updated"
+            }
+        else
+            render json: {
+                auth: false,
+                message: expense.errors.full_messages
             }
         end
     end
